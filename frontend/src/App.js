1e42b4807065ffa702070216b59b1b6925f62af9
DearAsiaNichole <asianichole87@gmail.com>
2023-02-15 16:41:53 -0500

    Google Authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,11 @@ import {Routes, Route, useNavigate }from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
 import {GoogleOAuthProvider} from '@react-oauth/google';
+
  
 const App = () => {
   return (
-    <GoogleOAuthProvider clientId='process.env.REACT_APP_GOOGLE_API_TOKEN'>
+    <GoogleOAuthProvider client_id='715016977346-6n4tjhdbi2kl65cd84er276e6u4ev71s.apps.googleusercontent.com'>
     <Routes>
         <Route path="login" element={<Login/>}/>
         <Route path="/*" element={<Home/>}/>
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,65 +1,68 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
-import {fcGoogle } from 'react-icons/fc';
-import {GoogleOAuthProvider} from '@react-oauth/google';
-
+import { fcGoogle } from 'react-icons/fc';
 import video from '../visuals/Video.mp4';
 import BlackLogo from '../visuals/BlackLogo.png';
-
-
 const Login = () => {
-  const responseGoogle=(response) =>{
-    
+/* {/* GOOGLE AUTHENTICATION }*/
+  const responseGoogle = (response) => {
+    console.log(response.credential);
   }
+  //NOTES:
+  //useEffect runs first but doesnt execute function untill after render gets inserted to the browser. 
+  //Then after rendering is done, useEffect determines what needs to be exicuted.
+  
+  useEffect(() => {
+    //NOTES:
+    //global google says that varible google is a "global variable" -- even though it's not declared anywhere in the program -- so it doesnt cause issues
+    //the script pasted in index.html (on line 29) deals with how the google variable behaves.
+
+    /*global google */
+    google.accounts.id.initialize({
+      client_id: "715016977346-6n4tjhdbi2kl65cd84er276e6u4ev71s.apps.googleusercontent.com",
+      callback: responseGoogle
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large"}
+    );
+  },[]);
+
+  
   return (
-    <div className= "flex justify-start items-center flex-col h-screen">
-      <div className= "relative w-full h-full">
-      {/* VIDEO SETTING FOR APP HOMEPAGE */}
+    
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+
+        {/* VIDEO SETTING FOR APP HOMEPAGE */}
         <video
-        src={video}
-        type="video/mp4"
-        Loop
-        controls={false}
-        muted
-        autoPlay
-        className="w-full h-full object-cover"
+          src={video}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
         />
 
         {/* ADDING A DARK OVERLAY OVER VIDEO AND INSERTING LOGO */}
-        < div className= "absolute flex flex-col justify-center top-0 right-0 left-0 bottom-0 bg blackOverlay">
+        < div className="absolute flex flex-col justify-center top-0 right-0 left-0 bottom-0 bg blackOverlay">
           <div className="p-5">
-          <img src={BlackLogo} width ="400px" alt="BlackLogo"/>
-          </div>     
+            <img src={BlackLogo} width="400px" alt="BlackLogo" /> 
+            <div id="signInDiv"></div>
+
+          </div>
+  
         </div>
 
-      {/* GOOGLE AUTHENTICATION */}
-        <div className="shadow-2xl">
-          <GoogleOAuthProvider
-          clientId='process.env.REACT_APP_GOOGLE_API_TOKEN'
+      
 
-          //call back functions 
-          render={(renderProps) =>(
 
-            <button 
-               type="button" 
-               className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-               onClick={renderProps.onClick}
-               disabled={renderProps.disabled}
-              >
-               <fcGoogle className="mr-4"/> Sign in with Google
-            </button>
-          )}
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-          cookiePolicy="single_host_origin"
-          />
+        <div className="shadow-2x1">
+
         </div>
-        
-        
-      <div className="shadow-2x1">
-
       </div>
-       </div> 
     </div>
   )
 }
