bec04e7022ee52867d74392ae3952df5709d3d3b
DearAsiaNichole <asianichole87@gmail.com>
2023-02-21 12:00:59 -0500

    New docs

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,12 +4,15 @@ import { fcGoogle } from 'react-icons/fc';
 import video from '../visuals/Video.mp4';
 import BlackLogo from '../visuals/BlackLogo.png';
 const Login = () => {
-/* {/* GOOGLE AUTHENTICATION }*/
+
+/* {/* GOOGLE AUTHENTICATION START}*/
   const responseGoogle = (response) => {
     console.log(response.credential);
   }
   //NOTES:
-  //useEffect runs first but doesnt execute function untill after render gets inserted to the browser. 
+  //useEffect HOOK: asyronous runs a function every render of the component -- is a way to run code on every render
+  //The component renders initially, and also when the state changes (re-renders to update)
+  //runs first but doesnt execute function until AFTRER render gets inserted to the browser. 
   //Then after rendering is done, useEffect determines what needs to be exicuted.
   
   useEffect(() => {
@@ -27,11 +30,15 @@ const Login = () => {
       document.getElementById("signInDiv"),
       { theme: "outline", size: "large"}
     );
-  },[]);
+  },[]); 
+  //,[]); <--- NOTES:
+         //the array is a Dependency:Having an empty array allows for the useEffect to only run once. a an error occurs
+         //If a variable if the dependency changes then it will reload the function
+         //useEffect with NO Dependenceies: will run at every time a component
 
+/* {/* GOOGLE AUTHENTICATION END}*/
   
   return (
-    
     <div className="flex justify-start items-center flex-col h-screen">
       <div className="relative w-full h-full">
 
@@ -45,20 +52,15 @@ const Login = () => {
           autoPlay
           className="w-full h-full object-cover"
         />
-
         {/* ADDING A DARK OVERLAY OVER VIDEO AND INSERTING LOGO */}
         < div className="absolute flex flex-col justify-center top-0 right-0 left-0 bottom-0 bg blackOverlay">
           <div className="p-5">
             <img src={BlackLogo} width="400px" alt="BlackLogo" /> 
+
+            {/* INSERTING GOOGLE SIGNIN BUTTON */}
             <div id="signInDiv"></div>
-
           </div>
-  
         </div>
-
-      
-
-
         <div className="shadow-2x1">
 
         </div>
