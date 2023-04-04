283ca2d012a8ef78cfb251a5fb1de8330341fc23
Madelyn Massett <massetm@sunypoly.edu>
2023-04-04 13:07:58 -0400

    created individual post display component

--- frontend/src/components/PostFeed.jsx
+++ frontend/src/components/PostFeed.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 import {client} from '../client';
+
 import WaterfallLayout from './WaterfallLayout';
 import Loadbars from './Loadbars';
 import {feedQuery, searchQuery} from '../utils/data';
@@ -16,13 +17,14 @@ const PostFeed = () => {
   // get posts - either all or based on artform search
   useEffect(() => {
     setLoading(true);
+    // get posts by artformID
     if (artformID) {
       const query = searchQuery(artformID);
       client.fetch(query).then((data) => {
         setPosts(data);
         setLoading(false);
       })
-    } else {
+    } else { // get all posts
       client.fetch(feedQuery).then((data) => {
         setPosts(data);
         setLoading(false);
@@ -34,6 +36,7 @@ const PostFeed = () => {
   // show loading message when fetching more posts
   if (loading) return <Loadbars message="Gathering new ideas..."/>
 
+  // show posts in waterfall layout
   return (
     <div>
       {posts && <WaterfallLayout posts={posts}/>}
